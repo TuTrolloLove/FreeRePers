7605855038a6be8ab0b917c67dc6a855761c5965
jose542.ceus <jose542.ceus@gmail.com>
2024-06-13 14:59:24 +0000

    Vercel Web Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+
 export default defineConfig({
-  integrations: [tailwind()]
   output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
+  integrations: [tailwind()]
 });
